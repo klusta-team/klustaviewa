70c7caee64f63e72b40f94c5287fd6a1961432b7
Max Hunter <nippoo@nippoo.net>
2013-07-25 18:37:23 +0100

    Basic implementation of data undersampling in rawdataview

--- klustaviewa/views/rawdataview.py
+++ klustaviewa/views/rawdataview.py
@@ -27,18 +27,83 @@ class RawDataManager(Manager):
     info = {}
     def set_data(self, rawdata=None, freq=None):
         
+        self.max_size = 500
+        
         samples = rawdata[:5000, :]
         position, shape = process_coordinates(samples.T)
+        xlim = 1., 20.
+        
+        self.shape = shape
+        self.freq = freq
+        self.duration = samples.shape[0]/self.freq
+        
+        xlimex, slice = self.get_view(shape[0], xlim, freq)
+        samples, bounds, size = self.get_undersampled_data(rawdata, xlimex, slice) 
         
         self.rawdata = rawdata
         self.samples = samples
         self.position = position
-        self.shape = shape
-        self.freq = freq
-        self.duration = samples.shape[0]/self.freq
         self.interaction_manager.get_processor('grid').update_viewbox()
         self.interaction_manager.activate_grid()
         print shape
+    
+    def get_view(self, total_size, xlim, freq): 
+        """Return the slice of the data.
+
+        Arguments:
+
+          * xlim: (x0, x1) of the window currently displayed.
+
+        """
+        # Viewport.
+        x0, x1 = xlim
+        d = x1 - x0
+        dmax = self.duration
+        zoom = max(dmax / d, 1.)
+        view_size = total_size / zoom
+        step = int(np.ceil(view_size / self.max_size))
+        # Extended viewport for data.
+        x0ex = np.clip(x0 - 3 * d, 0, dmax)
+        x1ex = np.clip(x1 + 3 * d, 0, dmax)
+        i0 = np.clip(int(np.round(x0ex * freq)), 0, total_size)
+        i1 = np.clip(int(np.round(x1ex * freq)), 0, total_size)
+        return (x0ex, x1ex), slice(i0, i1, step)
+            
+    def get_undersampled_data(self, data, xlim, slice):
+        duration_initial = 5
+        """
+        Arguments:
+    
+          * data: a HDF5 dataset of size Nsamples x Nchannels.
+          * xlim: (x0, x1) of the current data view.
+    
+        """
+        total_size = data.shape[0]
+        # Get the view slice.
+        # x0ex, x1ex = xlim
+        # x0d, x1d = x0ex / (duration_initial) * 2 - 1, x1ex / (duration_initial) * 2 - 1
+        # Extract the samples from the data (HDD access).
+        samples = data[slice, :]
+        # Convert the data into floating points.
+        samples = np.array(samples, dtype=np.float32)
+        # Normalize the data.
+        samples *= (1. / 65535)
+        # samples *= .25
+        # Size of the slice.
+        nsamples, nchannels = samples.shape
+        # Create the data array for the plot visual.
+        M = np.empty((nsamples * nchannels, 2))
+        samples = samples.T# + np.linspace(-1., 1., nchannels).reshape((-1, 1))
+        M[:, 1] = samples.ravel()
+        # Generate the x coordinates.
+        x = np.arange(slice.start, slice.stop, slice.step) / float(total_size - 1)
+        # [0, 1] -> [-1, 2*duration.duration_initial - 1]
+        x = x * 2 * self.duration / duration_initial - 1
+        M[:, 0] = np.tile(x, nchannels)
+        # Update the bounds.
+        bounds = np.arange(nchannels + 1) * nsamples
+        size = bounds[-1]
+        return M, bounds, size
         
 #    def update(self, data, xlimex, slice):
 #        samples, bounds, size = get_undersampled_data(data, xlimex, slice)
@@ -266,6 +331,7 @@ class RawDataInteractionManager(PlotInteractionManager):
     def initialize_default(self, constrain_navigation=None,
         momentum=True,
         ):
+        
         super(PlotInteractionManager, self).initialize_default()
         self.add_processor(NavigationEventProcessor,
             constrain_navigation=constrain_navigation, 
