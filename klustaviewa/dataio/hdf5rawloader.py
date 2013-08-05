eb4bf19140683ee310687e167aca849bfc5f42ff
Cyrille <cyrille@cyrille>
2013-08-05 16:09:46 +0100

    Added HDF5RawDataLoader stub.

---
+++ klustaviewa/dataio/hdf5rawloader.py
@@ -0,0 +1,11 @@
+class HDF5RawDataLoader(QtCore.QObject):
+    def open(self, filename):
+        # filename is blah.xxxx
+        # TODO: find the blah.klxd
+        filename = find_filename(filename, 'klxd')
+        pass
+
+    def get_raw_data(self):
+        # return slicable pytables object (EArray)
+        pass
+    
