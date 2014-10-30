Fix in the computation of the similarity matrix.

klustaviewa/stats/correlations.py
@@ -18,7 +18,7 @@ from kwiklib.utils.logger import warn
 # -----------------------------------------------------------------------------
 def compute_statistics(Fet1, Fet2, spikes_in_clusters, masks):
     """Return Gaussian statistics about each cluster."""
-    
+
     nPoints = Fet1.shape[0] #size(Fet1, 1)
     nDims = Fet1.shape[1] #size(Fet1, 2)
     # nclusters = Clu2.max() #max(Clu2)
@@ -27,7 +27,7 @@ def compute_statistics(Fet1, Fet2, spikes_in_clusters, masks):
     # Default masks.
     if masks is None:
         masks = np.ones((nPoints, nDims), dtype=np.float32)
-    
+
     # precompute the mean and variances of the masked points for each feature
     # contains 1 when the corresponding point is masked
     masked = np.zeros_like(masks)
@@ -49,7 +49,7 @@ def compute_statistics(Fet1, Fet2, spikes_in_clusters, masks):
     LogP = np.zeros((nPoints, nclusters))
 
     stats = {}
-    
+
     for c in spikes_in_clusters:
         # MyPoints = np.nonzero(Clu2==c)[0]
         MyPoints = spikes_in_clusters[c]
@@ -60,14 +60,14 @@ def compute_statistics(Fet1, Fet2, spikes_in_clusters, masks):
         # if len(MyPoints) > nDims:
         # LogProp = np.log(len(MyPoints) / float(nPoints)) # log of the proportion in cluster c
         Mean = np.mean(MyFet2, axis=0).reshape((1, -1))
-        
+
         if len(MyPoints) <= 1:
             CovMat = 1e-3*np.eye(nDims)
-            stats[c] = (Mean, CovMat, 1e3*np.eye(nDims), 
+            stats[c] = (Mean, CovMat, 1e3*np.eye(nDims),
                 (1e-3)**nDims, len(MyPoints))
             continue
-        
-        
+
+
         CovMat = np.cov(MyFet2, rowvar=0) # stats for cluster c
 
         # Variation Bayesian approximation
@@ -76,18 +76,18 @@ def compute_statistics(Fet1, Fet2, spikes_in_clusters, masks):
         CovMat += D * priorPoint  # D = np.diag(sigma2.ravel())
         CovMat /= (len(MyFet2) + priorPoint - 1)
 
-        
+
         # HACK: avoid instability issues, kind of works
         # CovMat += np.diag(1e-0 * np.ones(nDims))
-        
+
         # now, add the diagonal modification to the covariance matrix
         # the eta just for the current cluster
         etac = np.take(eta, MyPoints, axis=0)
-        d = np.sum(etac, axis=0) / nmasked
-        
+        d = np.mean(etac, axis=0)
+
         # Handle nmasked == 0
-        d[np.isnan(d)] = 0    
-        
+        d[np.isnan(d)] = 0
+
         # add diagonal
         CovMat += np.diag(d)
         # We don't compute that explicitely anymore: we solve Ax=b instead
@@ -101,45 +101,45 @@ def compute_statistics(Fet1, Fet2, spikes_in_clusters, masks):
         if _sign < 0:
             warn("The correlation matrix of cluster %d has a negative determinant (whaaat??)" % c)
 
-        
+
         stats[c] = (Mean, CovMat, CovMatinv, LogDet, len(MyPoints))
-        
+
     return stats
 
 def compute_correlations_approximation(features, clusters, masks,
         clusters_to_update=None, similarity_measure=None):
     """Compute the correlation matrix between every pair of clusters.
-    
+
     Use an approximation of the original Klusters grouping assistant, with
-    an integral instead of a sum (integral of the product of the Gaussian 
+    an integral instead of a sum (integral of the product of the Gaussian
     densities).
-    
+
     A dictionary pairs => value is returned.
-    
+
     Compute all (i, *) and (i, *) for i in clusters_to_update
-    
+
     """
     nPoints = features.shape[0] #size(Fet1, 1)
     nDims = features.shape[1] #size(Fet1, 2)
     c = np.unique(clusters)
-    
+
     spikes_in_clusters = dict([(clu, np.nonzero(clusters == clu)[0]) for clu in c])
     nclusters = len(spikes_in_clusters)
-    
+
     stats = compute_statistics(features, features, spikes_in_clusters, masks)
-    
+
     clusterslist = sorted(stats.keys())
-    
+
     # New matrix (clu0, clu1) => new value
     C = {}
 
     if clusters_to_update is None:
         clusters_to_update = clusterslist
-    
+
     # Update the new matrix on the rows and diagonals of the clusters to
     # update.
     for ci in clusters_to_update:
-        
+
         # WARNING: some cluster statistics may be missing, as we only
         # use a subset of all spikes when computing the similarity matrix
         # (to avoid loading all features from HDF5). If a cluster is
@@ -148,13 +148,13 @@ def compute_correlations_approximation(features, clusters, masks,
             for cj in clusterslist:
                 C[ci, cj] = C[cj, ci] = 0.
             continue
-                
+
         mui, Ci, Ciinv, logdeti, npointsi = stats[ci]
         for cj in clusterslist:
             muj, Cj, Cjinv, logdetj, npointsj = stats[cj]
-            
+
             dmu = (muj - mui).reshape((-1, 1))
-            
+
             # pij is the probability that mui belongs to Cj:
             #    $$p_{ij} = w_j * N(\mu_i | \mu_j; C_j)$$
             # where wj is the relative size of cluster j
@@ -171,9 +171,9 @@ def compute_correlations_approximation(features, clusters, masks,
 
             C[ci, cj] = wj * np.exp(logpij)[0,0]
 
-    
+
     return C
-    
+
 def get_similarity_matrix(dic):
     """Return a correlation matrix from a dictionary. Normalization happens
     here."""
@@ -182,36 +182,36 @@ def get_similarity_matrix(dic):
     nclusters = len(clusters)
     clumax = max(clusters) + 1
     matrix = np.zeros((nclusters, nclusters))
-    
+
     # Relative clusters: cluster absolute => cluster relative
     clusters_rel = np.zeros(clumax, dtype=np.int32)
     clusters_rel[clusters] = np.arange(nclusters)
-    
+
     for (clu0, clu1), value in dic.iteritems():
         matrix[clusters_rel[clu0], clusters_rel[clu1]] = value
-        
+
     return matrix
-    
+
 def normalize(matrix, direction='row'):
-    
+
     if direction == 'row':
         s = matrix.sum(axis=1)
     else:
         s = matrix.sum(axis=0)
-    
+
     # Non-null rows.
     indices = (s != 0)
-    
+
     # Row normalization.
     if direction == 'row':
         matrix[indices, :] *= (1. / s[indices].reshape((-1, 1)))
-    
+
     # Column normalization.
     else:
         matrix[:, indices] *= (1. / s[indices].reshape((1, -1)))
-    
+
     return matrix
-    
+
 
 compute_correlations = compute_correlations_approximation
 