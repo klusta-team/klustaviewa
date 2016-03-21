ee8ca51ed5f59c52a37f9bedb938d7df17eeaabd
Cyrille Rossant <cyrille.rossant@gmail.com>
2016-03-21 16:44:14 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,10 @@
 import os
-from setuptools import *
-
-try:
-    from Cython.Distutils import build_ext
-except ImportError:
-    use_cython = False
-else:
-    use_cython = True
+from setuptools import setup
 
 import numpy as np
 
 cmdclass = { }
 ext_modules = [ ]
-
-if use_cython:
-    ext_modules += [
-        Extension("klustaviewa.stats.correlograms_cython",
-            ["klustaviewa/stats/correlograms_cython.pyx"]),
-    ]
-    cmdclass.update({ 'build_ext': build_ext })
-else:
-    ext_modules += [
-        Extension("klustaviewa.stats.correlograms_cython",
-            ["klustaviewa/stats/correlograms_cython.c"]),
-    ]
-
 
 LONG_DESCRIPTION = """Spike sorting graphical interface."""
 
@@ -36,7 +16,7 @@ if __name__ == '__main__':
     setup(
         zip_safe=False,
         name='klustaviewa',
-        version='0.3.0',
+        version='0.4.0',
         author='Cyrille Rossant',
         author_email='rossant@github',
         packages=['klustaviewa',
@@ -52,32 +32,6 @@ if __name__ == '__main__':
                   'klustaviewa.wizard',
                   'klustaviewa.wizard.tests',
 
-                  #<
-                  # 'qtools',
-                  # 'qtools.qtpy',
-                  # 'qtools.tests',
-
-                  # 'kwiklib',
-                  # 'kwiklib.dataio',
-                  # 'kwiklib.dataio.tests',
-                  # 'kwiklib.scripts',
-                  # 'kwiklib.utils',
-                  # 'kwiklib.utils.tests',
-
-                  # 'spikedetekt2',
-                  # 'spikedetekt2.core',
-                  # 'spikedetekt2.core.tests',
-                  # 'spikedetekt2.processing',
-                  # 'spikedetekt2.processing.tests',
-
-                  # 'galry',
-                  # 'galry.managers',
-                  # 'galry.processors',
-                  # 'galry.test',
-                  # 'galry.visuals',
-                  # 'galry.visuals.fontmaps',
-                  # #>
-
                   ],
 
         # Scripts.
@@ -85,26 +39,13 @@ if __name__ == '__main__':
             'gui_scripts': [
                 'klustaviewa = klustaviewa.scripts.runklustaviewa:main',
                 ],
-                # 'kwikskope = klustaviewa.scripts.runkwikskope:main', ],
-            'console_scripts': [
-                'kwikkonvert = kwiklib.scripts.runkwikkonvert:main',
-                'klusta = spikedetekt2.core.script:main',
-                ],
         },
 
-        # scripts=[],
-
-        app=['klustaviewa/scripts/runklustaviewa.pyw',
-             'klustaviewa/scripts/runkwikskope.pyw',
-            ],
+        # app=['klustaviewa/scripts/runklustaviewa.pyw',
+        #     ],
 
         package_data={
             'klustaviewa': ['icons/*.png', 'icons/*.ico', 'gui/*.css'],
-
-            # # INCLUDE GALRY
-            # 'galry': ['cursors/*.png', 'icons/*.png'],
-            # 'galry.visuals': ['fontmaps/*.*'],
-            # 'galry.test': ['autosave/*REF.png'],
 
         },
 
